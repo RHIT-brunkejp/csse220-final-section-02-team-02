87c3950cd20387a4468a5f7b91170b919a968ae3
brunkejp <brunkejp@rose-hulman.edu>
2026-01-27 11:29:53 -0500

    Test Commit - JJ (Changed author in MainApp)

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -6,7 +6,7 @@ import ui.GameWindow;
 
 /**
  * Class: MainApp
- * @author Put your team name here
+ * @author Silly Syntax Sloths (Section 2 Group 2)
  * <br>Purpose: Top level class for CSSE220 Project containing main method 
  * Entry point for the final project.
  */
@@ -22,4 +22,5 @@ public class MainApp {
 		GameWindow.show();
 		// Hint: MainApp should not contain game logic or drawing code
 		}
+	
 }
