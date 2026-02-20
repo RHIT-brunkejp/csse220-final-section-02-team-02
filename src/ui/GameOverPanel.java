cb258e464410422c567a519fa5eed706c2a05fb7
brunkejp <brunkejp@rose-hulman.edu>
2026-02-20 01:02:56 -0500

    Revert "Level 1 Zombie Patrol Paths - JJ"
    This reverts commit fdc45f7dc16e0f528a831d84081381272c74d7f0.

--- src/ui/GameOverPanel.java
+++ src/ui/GameOverPanel.java
@@ -1,6 +1,7 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -10,13 +11,18 @@ public class GameOverPanel extends JPanel{
 	private JLabel label;
 	 public JButton button;
 	 public GameOverPanel() {
-		 label = new JLabel("Game Over");
-		 button = new JButton("Restart");
-		 this.setLayout(new BorderLayout());
-		 this.add(label, BorderLayout.CENTER);
-		 this.add(button, BorderLayout.SOUTH);
-		 
-	 }
+
+		    label = new JLabel("Game Over");
+		    label.setHorizontalAlignment(JLabel.CENTER);
+		    label.setFont(new Font("Arial", Font.BOLD, 40));
+
+		    button = new JButton("Restart");
+
+		    this.setLayout(new BorderLayout());
+		    this.add(label, BorderLayout.CENTER);
+		    this.add(button, BorderLayout.SOUTH);
+		}
+
 	 public void scoredisplay(int score) {
 		 this.label.setText("<html>Game OVER! Score: " + score);
 		 
